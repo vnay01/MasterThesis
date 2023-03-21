107262e9e07100a27b637c1a152052f14c37313c
vnay01 <vnay01@gmail.com>
2023-03-21 09:48:15 +0100

    Initial commit...

---
+++ test.v
@@ -0,0 +1,31 @@
+//// Module for initial tests
+
+module test( clk, reset, last, crc, data_in, data_out );
+
+    // Port direction
+
+    input clk;
+    input reset;
+    input last;
+    input crc;
+    input data_in;
+    output reg data_out;
+
+
+    // Behavioral description
+
+    always @(posedge clk) begin
+        if (!reset)
+        data_out <= 1'b0;
+        else if (last)
+        data_out <= 1'b0;
+        else if (crc)
+            data_out <= data_in;
+            else
+            data_out <= 1'b0;
+    end
+
+    initial begin
+        $display("Compilation...done");
+    end
+endmodule
