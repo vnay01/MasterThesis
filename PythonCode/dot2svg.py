35ffff8c873ada5c492404e23b96d23733ae2e77
vnay01 <vnay01@gmail.com>
2023-07-26 07:38:34 +0200

    Added dot to png converter code

---
+++ PythonCode/dot2svg.py
@@ -0,0 +1,16 @@
+### This module converts dot file into image format for easy visualization
+
+import sys
+import os
+
+def to_graph(fileName, target = ""):
+        if target == "" :
+            target = filename+".png"
+        command = "dot -Tpng " + fileName + " -o "+ target #Tsvg can be changed to Tjpg, Tpng, Tgif etc (see dot man pages)
+        os.system(command)
+if __name__=="__main__":
+    if len(sys.argv) != 2:
+        print ("\nUsage: python dot2svg.py mygraph.dot")
+        exit ()
+    filename=sys.argv[1]
+    to_graph(filename)
